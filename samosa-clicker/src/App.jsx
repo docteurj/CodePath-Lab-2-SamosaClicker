907cb1bbc946e6a18ea4370b07663882887a1c58
docteurj <jonathandocteur@gmail.com>
2023-02-28 10:45:21 -0500

    Updated Image

--- samosa-clicker/src/App.jsx
+++ samosa-clicker/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState } from 'react';
+import samosa_img from './assets/samosa.png'
 
 
 const App = () => {
@@ -30,7 +31,7 @@ const App = () => {
     <div className="App">
       <h1> Samosa Selector </h1>
       <h2> Count: {count} </h2>
-      <img className='samosa' src='https://toppng.com/uploads/preview/transparent-samosas-for-your-blog-frozen-samosa-11562922780nxujrishko.png' onClick={updateCount}></img>
+      <img className='samosa' src={samosa_img} onClick={updateCount}></img>
       <div className='container'>
         <div className='upgrade'>
           <h3>Double Stuffed 👯‍♀️</h3>
