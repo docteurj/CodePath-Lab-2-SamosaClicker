ad0e22d7d4c9385cb20144769098b402def9ae36
docteurj <jonathandocteur@gmail.com>
2023-02-28 09:51:17 -0500

    Title, Count, and Styling Complete

---
+++ samosa-clicker/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1> Samosa Selector </h1>
+      <h2> Count:</h2>
+      <img className='samosa' src='https://toppng.com/uploads/preview/transparent-samosas-for-your-blog-frozen-samosa-11562922780nxujrishko.png'></img>
+    </div>
+  )
+}
+
+export default App
