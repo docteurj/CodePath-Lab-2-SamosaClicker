486d0785910f9aaa5a2e50af3abe8e2a85894271
docteurj <jonathandocteur@gmail.com>
2023-02-28 10:27:16 -0500

    Counter Completed

--- samosa-clicker/src/App.jsx
+++ samosa-clicker/src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css';
+import { useState } from 'react';
+
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+  const updateCount = () => setCount(count + multiplier);
+
 
   return (
     <div className="App">
       <h1> Samosa Selector </h1>
-      <h2> Count:</h2>
-      <img className='samosa' src='https://toppng.com/uploads/preview/transparent-samosas-for-your-blog-frozen-samosa-11562922780nxujrishko.png'></img>
+      <h2> Count: {count} </h2>
+      <img className='samosa' src='https://toppng.com/uploads/preview/transparent-samosas-for-your-blog-frozen-samosa-11562922780nxujrishko.png' onClick={updateCount}></img>
     </div>
   )
 }
+
+
 
 export default App
